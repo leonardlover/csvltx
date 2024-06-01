67843bde197dc8bb027834e5d6476d75faaaaff2
Leonardo Lovera <leonard.lover@gmail.com>
2024-05-31 20:15:16 -0400

    feat: added command line arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::process;
+
+fn config(
+    mut args: impl Iterator<Item = String>,
+) -> Result<String, &'static str> {
+    args.next();
+
+    match args.next() {
+        Some(arg) => Ok(arg),
+        None => Err("Didn't get a file path"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = config(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 }
