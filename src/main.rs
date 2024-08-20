1139ef0c71e983aff430f16c627e8f039df7f483
Leonardo Lovera <leonard.lover@gmail.com>
2024-08-20 11:28:34 -0400

    feat: added unit support

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::process;
 
 fn config(
     mut args: impl Iterator<Item = String>,
-) -> Result<(String, String), &'static str> {
+) -> Result<(String, String, String), &'static str> {
     args.next();
 
     match args.next() {
@@ -15,7 +15,12 @@ fn config(
                     name.pop();
                     let name = name.join(".");
 
-                    Ok((contents, name))
+                    let unit = match args.next() {
+                        Some(arg) => arg,
+                        None => return Err("Didn't get a unit"),
+                    };
+
+                    Ok((contents, name, unit))
                 },
                 _ => Err("Could not open file"),
             }
@@ -25,7 +30,7 @@ fn config(
 }
 
 fn main() {
-    let (file_contents, file_name) = config(env::args()).unwrap_or_else(|err| {
+    let (file_contents, file_name, file_unit) = config(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -40,7 +45,10 @@ fn main() {
 
         println!("\\begin{{tabular}}{{{c}|}}\n    \\hline");
 
-        println!("    \\multicolumn{{{}}}{{|c|}}{{\\texttt{{{}}}}} \\\\\n    \\hline", headers.len(), file_name.replace('_', "\\_"));
+        println!("    \\multicolumn{{{}}}{{|c|}}{{\\texttt{{{}}}}} \\\\\n    \\hline",
+            headers.len(),
+            format!("{} [{}]", file_name.replace('_', "\\_"), file_unit),
+        );
 
         let header = headers.join(" & ").replace('_', "\\_");
 
